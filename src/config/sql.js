8f2f841de09f0f209cb297879e6fad29d7c05e6b
Nika Nozadze <nikanozadze1010@gmail.com>
2023-07-21 20:41:54 +0400

    feat: project setup

---
+++ src/config/sql.js
@@ -0,0 +1,16 @@
+import pgk from "pg";
+const { Pool } = pgk;
+
+const pool = new Pool({
+  host: "dpg-citaui5gkuvoiga7ens0-a",
+  port: 5432,
+  database: "products_0w2h",
+  user: "products_0w2h_user",
+  password: "5n7lk5O1uNnA01GrXgO8HMqTjNpnvc9D",
+});
+
+export const createTable = async () => {
+  return await pool.query(
+    "CREATE TABLE IF NOT EXIST products(id SERIAL PRiMARY KEY, title TEXT, price INT)"
+  );
+};
