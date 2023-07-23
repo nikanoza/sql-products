5d48f5fcbdab507b7b543e6dce7eb681ce3736f6
Nika Nozadze <nikanozadze1010@gmail.com>
2023-07-23 23:14:13 +0400

    feat: create update route

--- src/routes/product-router.js
+++ src/routes/product-router.js
@@ -3,6 +3,7 @@ import {
   createProduct,
   deleteProduct,
   getAllProducts,
+  updateProduct,
 } from "../controllers/products-controller.js";
 
 const productRouter = express.Router();
@@ -10,5 +11,6 @@ const productRouter = express.Router();
 productRouter.get("/products", getAllProducts);
 productRouter.post("/products", createProduct);
 productRouter.delete("/products/:id", deleteProduct);
+productRouter.put("/products/:id", updateProduct);
 
 export default productRouter;
